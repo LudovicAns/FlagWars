75317f8ae5646d02685f2fb4ea6da384592e66a2
LudovicAns <ludovicanselin@gmail.com>
2023-01-11 14:41:32 +0100

    Initial commit

---
+++ src/main/java/fr/ludovicans/flagwars/FlagWars.java
@@ -0,0 +1,17 @@
+package fr.ludovicans.flagwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FlagWars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
